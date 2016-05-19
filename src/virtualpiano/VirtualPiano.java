0299f4f77f182c39766f0fdf117b21a6011cfa85
erafkin <erafkin@10.152.50.194>
2016-05-19 14:47:40 -0500

    added the reverse skin style sheet

--- src/virtualpiano/VirtualPiano.java
+++ src/virtualpiano/VirtualPiano.java
@@ -22,7 +22,6 @@ public class VirtualPiano extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
